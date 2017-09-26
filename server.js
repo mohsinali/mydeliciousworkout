485660c28618faa612a0de33c45587131eaa9db9
Mohsin Ali <mohsin@Mohsins-MacBook-Pro.local>
2017-09-26 09:15:33 +0500

    First commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('home.hbs');
+});
+
+app.listen(3000);
