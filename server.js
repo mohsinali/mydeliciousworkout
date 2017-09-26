6ebe54d0881299a9464f684ac2873e8036bd3a53
Mohsin Ali <mohammadmohsin@gmail.com>
2017-09-26 09:39:58 +0500

    excluding bootstrap & jquery from npm packages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
+app.use(require('express-jquery')('/jquery.js'));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
