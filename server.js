3da9d148403b54985851f34b5b2503bab3145599
Mohsin Ali <mohammadmohsin@gmail.com>
2017-09-29 13:24:09 +0500

    Basic navigation

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const hbs = require('hbs');
 const port = process.env.PORT || 3000;
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   res.render('home.hbs');
+});
+
+app.get('/workout/:day', (req, res) => {
+  res.render('monday.hbs');
 });
 
 app.listen(port, () =>{
